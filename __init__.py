a3ec4e70888f6398c74858dec5f52e40362c24a4
pipe <pipe08742509123@javerianacali.edu.co>
2023-10-31 22:48:48 -0500

    agregamos potencia

--- __init__.py
+++ __init__.py
@@ -16,6 +16,10 @@ def divide(num_1, num_2):
     result = num_1 / num_2
     return result
 
+def power(num_1, num_2):
+    result = num_1 ** num_2
+    return result
+
 def game():
     score = 0
     while True:
@@ -23,22 +27,25 @@ def game():
         print('1. Add')
         print('2. Subtract')
         print('3. Multiply')
-        print('4. Divide')  # Agregamos la opción de división
+        print('4. Divide')
+        print('5. Power')  # Agregamos la opción de potencia
         print('0. Exit')
         option = int(input('\nChoose an option: '))
         if option == 0:
             break
-        num_1 = int(input('Enter the first number: '))
-        num_2 = int(input('Enter the second number: '))
-        answer = float(input('Enter your answer: '))  # Usamos float para admitir números decimales en la división
+        num_1 = float(input('Enter the first number: '))
+        num_2 = float(input('Enter the second number: '))
+        answer = float(input('Enter your answer: '))
         if option == 1:
             result = add(num_1, num_2)
         elif option == 2:
             result = subtract(num_1, num_2)
         elif option == 3:
             result = multiply(num_1, num_2)
-        elif option == 4:  # Si se elige la opción 4, utilizamos la función de división
+        elif option == 4:
             result = divide(num_1, num_2)
+        elif option == 5:  # Si se elige la opción 5, utilizamos la función de potencia
+            result = power(num_1, num_2)
         if result == answer:
             score += 1
             print('Correct!!')
